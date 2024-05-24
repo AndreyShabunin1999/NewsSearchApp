082d18ed50e06ae1321854e57a7edc63ecefaeac
Андрей <81184064+AndreyShabunin1999@users.noreply.github.com>
2024-05-24 17:05:34 +0300

    Начато создание моделей для сетевых запросов

--- newsapi/src/main/java/com/anshabunin/newsapi/NewsApi.kt
+++ newsapi/src/main/java/com/anshabunin/newsapi/NewsApi.kt
@@ -1,4 +1,23 @@
 package com.anshabunin.newsapi
 
-class NewsApi {
+import androidx.annotation.IntRange
+import com.anshabunin.newsapi.models.Language
+import com.anshabunin.newsapi.models.SortBy
+import retrofit2.http.GET
+import retrofit2.http.Query
+import java.util.Date
+
+interface NewsApi {
+    @GET("/everything")
+    fun everything(
+        @Query("q") query: String? = null,
+        @Query("from") date: Date? = null,
+        @Query("to") to: Date? = null,
+        @Query("languages") languages: List<Language>? = null,
+        @Query("sortBy") sortBy: SortBy? = null,
+        @Query("pageSize") @IntRange(from = 0, to = 100) pageSize: Int? = 100,
+        @Query("page") @IntRange(from = 1) page: Int? = 1,
+    )
 }
+
+
---
+++ newsapi/src/main/java/com/anshabunin/newsapi/models/Article.kt
@@ -0,0 +1,7 @@
+package com.anshabunin.newsapi.models
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+class Article {
+}
